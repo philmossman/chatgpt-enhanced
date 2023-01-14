0a914438f96c629809ff1de41d33dc09a2675de6
Phil Mossman <mossmanpik@iname.com>
2023-01-14 12:14:18 +0000

    Frontend and backend taking to each other

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const configuration = new Configuration({
   organization: "org-rG2TQVKqo01acJruNsbSJYwH",
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
-// const response = await openai.listEngines();
 
-async function callApi() {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
-  });
-  console.log(response.data.choices[0].text);
-}
+// add body parser and cors to express
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// const response = await openai.listEngines();
 
 // create a simple express api that calls the function above
 
-const app = express();
 const port = 3080;
 
 app.post("/", async (req, res) => {
+  const { message } = req.body;
+  console.log(message, "message");
+
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
   });
-  console.log(response.data.choices[0].text);
   res.json({
-    data: response.data,
+    message: response.data.choices[0].text,
   });
 });
 
